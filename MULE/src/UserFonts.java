b4ddeaf1c7aae8345ab766a1139ed795b1b6602e
Ziyi Jiang <zjiang42@gatech.edu>
2014-01-17 23:40:30 -0500

    Create UserFonts.java

---
+++ MULE/src/UserFonts.java
@@ -0,0 +1,16 @@
+package ui_components;
+
+import java.awt.Font;
+
+import org.newdawn.slick.TrueTypeFont;
+
+/**
+ * Sets the font for the user
+ * 
+ * @author Ziyi
+ * 
+ */
+public class UserFonts {
+	private static Font mule = new Font("Impact", Font.PLAIN, 14);
+	public static TrueTypeFont fntMule = new TrueTypeFont(mule, true);
+}
